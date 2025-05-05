a76e1cda122a7f4f2bc414ed5468ba107d8936c6
gabsvelozo <gabsmastellari@gmail.com>
2025-05-05 13:49:13 -0300

    testes corretos 1

--- src/main/java/br/edu/cs/poo/ac/seguro/daos/ApoliceDAO.java
+++ src/main/java/br/edu/cs/poo/ac/seguro/daos/ApoliceDAO.java
@@ -1,21 +1,40 @@
 package br.edu.cs.poo.ac.seguro.daos;
+
+import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
 import br.edu.cs.poo.ac.seguro.entidades.Apolice;
 
+import java.io.Serializable;
+
 public class ApoliceDAO extends DAOGenerico {
-    public Apolice buscar(String numero) {
-
-        return null;
+    public ApoliceDAO() {
+        cadastro = new CadastroObjetos(Apolice.class);
     }
-    public boolean incluir(Apolice segurado) {
+    public Apolice buscar(String numero) {
+        return (Apolice)cadastro.buscar(numero);
+    }
 
-        return false;
+    public boolean incluir(Apolice segurado) {
+        if (buscar(segurado.getNumero()) != null) {
+            return false;
+        } else {
+            cadastro.incluir((Serializable)segurado, segurado.getNumero());
+            return true;
+        }
     }
     public boolean alterar(Apolice segurado) {
-
-        return false;
+        if (buscar(segurado.getNumero()) == null) {
+            return false;
+        } else {
+            cadastro.alterar((Serializable)segurado, segurado.getNumero());
+            return true;
+        }
     }
     public boolean excluir(String numero) {
-
-        return false;
+        if (buscar(numero) == null) {
+            return false;
+        } else {
+            cadastro.excluir(numero);
+            return true;
+        }
     }
 }
